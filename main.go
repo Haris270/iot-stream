7a99cbd0aec22dc25f1f8b295beb9ee286412ebe
Muhammad Haris <m.haris425@gmail.com>
2026-04-04 12:15:30 -0400

    add SensorData struct

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
 package main
+
+import "time"
+
+type SensorData struct {
+	Device_id   int       `json:"device_id"`
+	Temperature float64   `json:"temperature"`
+	Rpm         int       `json:"rpm"`
+	Time_stamp  time.Time `json:"time_stamp"`
+}
