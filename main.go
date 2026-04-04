a58d79985c6b58e219c40ddef3f5821d414c8b6e
Muhammad Haris <m.haris425@gmail.com>
2026-04-04 12:51:20 -0400

    setting up main func

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "time"
+import (
+	"context"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 type SensorData struct {
 	Device_id   int       `json:"device_id"`
 	Temperature float64   `json:"temperature"`
 	Rpm         int       `json:"rpm"`
 	Time_stamp  time.Time `json:"time_stamp"`
+}
+
+func sendTelemetry(ctx context.Context, id int, wg *sync.WaitGroup) {
+	var data SensorData = SensorData{
+		id,
+		(rand.Float64() * 25) + 25,
+		rand.Intn(25) + 50,
+		time.Now()}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go sendTelemetry(ctx, rand.Intn(100), &wg)
+	}
+
+	wg.Wait()
+
 }
